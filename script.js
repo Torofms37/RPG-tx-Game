9e3951fefb8ef4fdbce6463e7283a01e4c5e78fe
torofms37 <torofms37@gmail.com>
2025-02-05 22:42:49 -0700

    aa

--- script.js
+++ script.js
@@ -190,7 +190,8 @@ function goFight() {
 }
 
 function attack() {
-  text.innerText = "The " + monsters[fighting].name + " attacks." + monsters[fighting].name;
+  text.innerText = "The " + monsters[fighting].name + " attacks." + text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+
 }
 
 function dodge() {
